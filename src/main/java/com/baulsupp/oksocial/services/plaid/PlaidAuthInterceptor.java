4254c4930386e2ee0ae706b60bd7e5804585b7ff
Yuri Schimke <yschimke@fb.com>
2016-12-06 23:19:19 +0000

    plaid

--- src/main/java/com/baulsupp/oksocial/services/plaid/PlaidAuthInterceptor.java
+++ src/main/java/com/baulsupp/oksocial/services/plaid/PlaidAuthInterceptor.java
@@ -6,31 +6,49 @@ import com.baulsupp.oksocial.credentials.ServiceDefinition;
 import com.baulsupp.oksocial.output.OutputHandler;
 import com.baulsupp.oksocial.secrets.Secrets;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
-import okhttp3.HttpUrl;
+import okhttp3.FormBody;
 import okhttp3.Interceptor;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okio.Buffer;
+import org.eclipse.jetty.util.UrlEncoded;
 
 public class PlaidAuthInterceptor implements AuthInterceptor<PlaidCredentials> {
+  public static final MediaType FORM_DATA = MediaType.parse("application/x-www-form-urlencoded");
+
   @Override public Response intercept(Interceptor.Chain chain, PlaidCredentials credentials)
       throws IOException {
     Request request = chain.request();
 
-    HttpUrl newUrl = request.url()
-        .newBuilder()
-        .addQueryParameter("client_id", credentials.clientId)
-        .addQueryParameter("secret", credentials.secret)
-        .build();
+    if (!request.method().equals("GET") && request.body().contentType().equals(FORM_DATA)) {
+      FormBody.Builder newBody = new FormBody.Builder().add("client_id", credentials.clientId)
+          .add("secret", credentials.secret);
 
-    request =
-        request.newBuilder().url(newUrl).build();
+      Buffer buffer = new Buffer();
+      request.body().writeTo(buffer);
+
+      UrlEncoded e = new UrlEncoded();
+      e.decode(buffer.readString(request.body().contentType().charset(StandardCharsets.UTF_8)));
+
+      for (Map.Entry<String, List<String>> entry: e.entrySet()) {
+        for (String v: entry.getValue()) {
+          newBody.add(entry.getKey(), v);
+        }
+      }
+
+      request = request.newBuilder().method(request.method(), newBody.build()).build();
+    }
 
     return chain.proceed(request);
   }
